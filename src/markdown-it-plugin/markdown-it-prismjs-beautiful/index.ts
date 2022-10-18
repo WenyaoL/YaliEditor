678c0929f61fde658e411e6a9609284f15323295
liangwy <835946631@qq.com>
2022-10-18 23:01:51 +0800

    update

--- src/markdown-it-plugin/markdown-it-prismjs-beautiful/index.ts
+++ src/markdown-it-plugin/markdown-it-prismjs-beautiful/index.ts
@@ -4,8 +4,63 @@
  * @github https://github.com/WenyaoL/YaliEditor
  */
 
-import Prism from 'prismjs'
+ import Prism from 'prismjs'
+ import initLoadLanguage, { loadedLanguages, transformAliasToOrigin } from './loadLanguage'
+ import { languages } from 'prismjs/components.js'
+ 
+ const prism = Prism
+ window.Prism = Prism
+ /* eslint-disable */
+ import('prismjs/plugins/keep-markup/prism-keep-markup')
+ /* eslint-enable */
+ const langs = []
+ 
+ for (const name of Object.keys(languages)) {
+   const lang = languages[name]
+   langs.push({
+     name,
+     ...lang
+   })
+   if (lang.alias) {
+     if (typeof lang.alias === 'string') {
+       langs.push({
+         name: lang.alias,
+         ...lang
+       })
+     } else if (Array.isArray(lang.alias)) {
+       langs.push(...lang.alias.map(a => ({
+         name: a,
+         ...lang
+       })))
+     }
+   }
+ }
+ 
+ const loadLanguage = initLoadLanguage(Prism)
+ 
+ // pre load latex and yaml and html for `math block` \ `front matter` and `html block`
+ loadLanguage('latex')
+ loadLanguage('yaml')
 
-export const prismHighlighter = (str:string,lang:string)=>{
+export const prismHighlighter =(str:string,lang:string)=>{
+    let preCode = ""
+    let lowerCaseLang = lang.toLowerCase()
+    //loadLanguages(lowerCaseLang)
+    loadLanguage(lowerCaseLang)
+    let gammer = Prism.languages[lowerCaseLang]
+    console.log("要加载的语言",lowerCaseLang);
+    console.log("加载的gammer",gammer);
+    //console.log(java);
+    console.log(Prism.languages.javascript);
+    if(gammer){
+        let html = Prism.highlight(str,gammer,lowerCaseLang)
+        html = '<pre class="line-numbers"><code>' + html + '</code></pre>'
+        return html
+    }
+    
+    
+    return '<pre class="line-numbers"><code>' + str + '</code></pre>'
     
 }
+
+export default prismHighlighter
