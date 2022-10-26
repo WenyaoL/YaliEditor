ae220cf34ccd3489d4900d46bcb4b5bb24428527
liangwy <835946631@qq.com>
2022-10-27 02:58:52 +0800

    更换一套更加清晰的ICO图片

--- vue.config.js
+++ vue.config.js
@@ -35,21 +35,21 @@ module.exports = defineConfig({
               ext: ["md","markdown","mmd","mdown","mdtxt","mdtext"],
               name: "Markdown",
               description: "Markdown document",
-              icon: "./resource/icons/md.ico"
+              icon: "./resource/icons/md.png"
         },
         nsis: { //nsis安装器配置
           oneClick: false, // 是否需要点击安装，自动更新需要关掉
           allowToChangeInstallationDirectory:true, //是否能够选择安装路径
           //installerIcon:"./resource/icons/yali.ico",// 安装程序图标（最好用256 × 256以上的图标）
           //uninstallerIcon:"./resource/icons/yali.ico",//卸载程序图标（最好用256 × 256以上的图标）
-          perMachine: true, // 是否需要辅助安装页面
-          createDesktopShortcut: true, // 创建桌面图标
-          createStartMenuShortcut: true,// 创建开始菜单图标
+          perMachine: false, 
+          //createDesktopShortcut: true, // 创建桌面图标
+          //createStartMenuShortcut: true,// 创建开始菜单图标
           //license:"./src/license/license.html" //安装界面的软件许可证，如果不配置，不会出现软件许可证界面
         },
         win:{
           artifactName: "yali-${arch}-win.${ext}",
-          icon: './resource/icons/yali.ico',
+          icon: './resource/icons/yali.png',
           target: [
             {
               target: "nsis", // 输出目录的方式
@@ -60,7 +60,7 @@ module.exports = defineConfig({
               arch: ["x64","ia32"]
             }*/
             ],
-          requestedExecutionLevel: "asInvoker"
+          //requestedExecutionLevel: "asInvoker"
         },
         /*electronDownload: {
           customDir: `v13.0.0`,
