c6631ff3863a094b4219c476f55bf849fbaa3d72
liangwy <835946631@qq.com>
2023-02-09 23:44:46 +0800

    修复图片undefined问题

--- src/markdown-it-plugin/util/formatText.ts
+++ src/markdown-it-plugin/util/formatText.ts
@@ -1,36 +1,38 @@
 
 
-export const toImgText = (info:string,src:string)=>{
-    "![" + info +"](" + src + ")"
+export const toImgText = (info: string, src: string) => {
+    "![" + info + "](" + src + ")"
 }
 
-export const toImgElementText = (info:string,src:string)=>{
+export const toImgElementText = (info: string, src: string) => {
+    src = src ? src : ''
+    info = info ? info : ''
     const pre = `<span class="md-border">![</span>`
     const mid = `<span class="md-border">](</span>`
     const suf = `<span class="md-border">)</span>`
-    info = '<span class="md-info md-meta">' + info +  '</span>'
-    src = '<span class="md-img-url md-meta">' + src +  '</span>'
+    info = '<span class="md-info md-meta">' + info + '</span>'
+    src = '<span class="md-img-url md-meta">' + src + '</span>'
     return pre + info + mid + src + suf
 }
 
-export function toKeyText(event: KeyboardEvent){
+export function toKeyText(event: KeyboardEvent) {
     let res = ""
-    if(event.ctrlKey){
+    if (event.ctrlKey) {
         res += "ctrl"
     }
-    
-    if(event.shiftKey){
-        res +="+"
+
+    if (event.shiftKey) {
+        res += "+"
         res += "shift"
     }
 
-    if(event.altKey){
-        res +="+"
+    if (event.altKey) {
+        res += "+"
         res += "alt"
     }
 
-    if(event.key){
-        res +="+"
+    if (event.key) {
+        res += "+"
         res += event.key
     }
 
